dcb9d631b439b609ada188bbae51e21c8f564ad0
ArsalaBangash <arsalabangash@gmail.com>
2020-01-29 12:08:45 -0500

    Began working on minimum implementation

---
+++ planner.js
@@ -0,0 +1,26 @@
+// Given a set of courses, is there a valid timetable?
+// What is a valid timetable?
+// A valid timetable has no conflicts
+
+// Next step is to determine if there is a conflict, given a set of courses
+
+// Notes
+// We'll be using info from the meeting sections field of the course JSON
+// The start and end times are in seconds from 12:00am 
+
+
+//To check conflict, compare the start time of each course to see if it is overlapped by other courses:
+// A function that determine whether overlapping exists
+// input: A set of course objects with its corresponding start and end time
+// dic = {"CSC108H5F2019":["Monday":[ "61400", "72800"], "Wednesday":["50400", "54000"],["Friday", "50400", "54000"]], 
+// "CSC318H5S"
+
+
+
+// If I'm looking at a course json
+// Determine the types of unique meeting sections
+// Create a configuration
+
+
+
+
